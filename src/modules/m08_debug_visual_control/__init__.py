c05b14b64ec2a5b7832f31c085cbadd75ba82a29
Uvaltus157 <dia727345@gmail.com>
2026-05-25 23:20:24 +0300

    Export M8 debug registry and status schema

---
+++ src/modules/m08_debug_visual_control/__init__.py
@@ -0,0 +1,11 @@
+from .module_registry import MODULE_BY_ID, MODULE_BY_PACKAGE, MODULE_REGISTRY, ModuleRegistryEntry
+from .status_schema import ModuleDebugStatus, ModuleDebugStatusPayload
+
+__all__ = [
+    "MODULE_BY_ID",
+    "MODULE_BY_PACKAGE",
+    "MODULE_REGISTRY",
+    "ModuleRegistryEntry",
+    "ModuleDebugStatus",
+    "ModuleDebugStatusPayload",
+]
