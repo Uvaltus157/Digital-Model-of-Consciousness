f77bd610959d51d96c50d9e100f1f82a1f7c4bbe
Uvaltus157 <dia727345@gmail.com>
2026-05-27 00:27:17 +0300

    Export canonical ConsciousDreamer API from M5 models package

---
+++ src/modules/m05_world_model_attention_workspace/models/__init__.py
@@ -0,0 +1,19 @@
+from src.modules.m05_world_model_attention_workspace.models.conscious_dreamer import (
+    CONSCIOUS_DREAMER_IMPLEMENTATION_LAYER,
+    CONSCIOUS_DREAMER_MODEL_VERSION,
+    ConsciousDreamer,
+    ConsciousDreamerConfig,
+    ConsciousDreamerLatest,
+    ConsciousDreamerLatestConfig,
+    make_conscious_dreamer_config_from_world,
+)
+
+__all__ = [
+    "ConsciousDreamer",
+    "ConsciousDreamerConfig",
+    "ConsciousDreamerLatest",
+    "ConsciousDreamerLatestConfig",
+    "make_conscious_dreamer_config_from_world",
+    "CONSCIOUS_DREAMER_MODEL_VERSION",
+    "CONSCIOUS_DREAMER_IMPLEMENTATION_LAYER",
+]
