a08f40be953176872c053f0a6ef29c02d684b001
Uvaltus157 <dia727345@gmail.com>
2026-05-30 21:10:31 +0300

    Export M10 global conscious broadcast module

---
+++ src/modules/m10_global_conscious_broadcast/__init__.py
@@ -0,0 +1,15 @@
+from src.modules.m10_global_conscious_broadcast.broadcast_gate import (
+    GlobalBroadcastConfig,
+    GlobalConsciousBroadcastGate,
+    pad_or_trim_broadcast,
+)
+from src.modules.m10_global_conscious_broadcast.broadcast_runtime import (
+    GlobalBroadcastRuntimeMixin,
+)
+
+__all__ = [
+    "GlobalBroadcastConfig",
+    "GlobalConsciousBroadcastGate",
+    "GlobalBroadcastRuntimeMixin",
+    "pad_or_trim_broadcast",
+]
